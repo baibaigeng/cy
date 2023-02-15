fc87a36c0ed1e43114bdd010ae30b1819250a2d3
baibaigeng <99377501+baibaigeng@users.noreply.github.com>
2023-02-15 16:02:06 +0800

    Update demo.cy.js

--- cypress/projects/gwm/e2e/demo.cy.js
+++ cypress/projects/gwm/e2e/demo.cy.js
@@ -4,11 +4,7 @@ describe("登录后的界面",()=>{
         cy.TESTlogin()
     })
     it('测试主页',()=>{
-        cy.get('.ivu-menu-light > :nth-child(3) > :nth-child(1)').click()
-        cy.get('.header-lang.ivu-dropdown > .ivu-dropdown-rel > div').click()
-        cy.get('.header-lang.ivu-dropdown > .ivu-select-dropdown > .ivu-dropdown-menu > :nth-child(3)').click()
-        cy.get('[style=""] > :nth-child(4) > .ivu-menu-submenu-title').click()
-        cy.get(':nth-child(2) > .ivu-menu-opened > .ivu-menu > :nth-child(2)').click()
+        cy.get('[style="text-align: right;"] > .arco-space > :nth-child(2) > .arco-btn').click()
         //切换为主页
     })
 
