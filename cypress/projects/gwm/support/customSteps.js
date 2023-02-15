6e29ed25b613dd579ab9b5c0ec1fcdb32ef3861d
baibaigeng <99377501+baibaigeng@users.noreply.github.com>
2023-02-15 15:51:25 +0800

    Update customSteps.js

--- cypress/projects/gwm/support/customSteps.js
+++ cypress/projects/gwm/support/customSteps.js
@@ -1,17 +1,14 @@
 import GWM_ELEMENT from "../fixtures/element.json"
 
 //登录
-Cypress.Commands.add('GWMlogin',() => {
+Cypress.Commands.add('TESTlogin',() => {
     cy.fixture('data').then((data) => {
     //使用箭头符号可以直接引用data
-      cy.visit(data.url, { timeout: 300000 })
+      cy.visit(data.url)
       cy.get(GWM_ELEMENT.longinUsername).type(data.username)
       cy.get(GWM_ELEMENT.loginPassword).type(data.password)
-      cy.loginMove()
-      cy.get(GWM_ELEMENT.loginnum).type(data.num)
       cy.get(GWM_ELEMENT.loginbutton).click()
       cy.url().should('include','welcome')
       cy.get('.header-a').should('contain', 'baigeng')
       })
   })
-  
