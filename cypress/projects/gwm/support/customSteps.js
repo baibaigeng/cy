dd76706c579059fc83e8596d2334a1c0a8da6db9
baibaigeng <99377501+baibaigeng@users.noreply.github.com>
2023-02-15 13:23:14 +0800

    Update customSteps.js

--- cypress/projects/gwm/support/customSteps.js
+++ cypress/projects/gwm/support/customSteps.js
@@ -4,7 +4,7 @@ import GWM_ELEMENT from "../fixtures/element.json"
 Cypress.Commands.add('GWMlogin',() => {
     cy.fixture('data').then((data) => {
     //使用箭头符号可以直接引用data
-      cy.visit(data.url)
+      cy.visit(data.url, { timeout: 300000 })
       cy.get(GWM_ELEMENT.longinUsername).type(data.username)
       cy.get(GWM_ELEMENT.loginPassword).type(data.password)
       cy.loginMove()
