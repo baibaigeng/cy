4558a6fa99f29056cbd7303275f15d7ce5b68e25
baibaigeng <99377501+baibaigeng@users.noreply.github.com>
2023-02-15 15:59:19 +0800

    Update customSteps.js

--- cypress/projects/gwm/support/customSteps.js
+++ cypress/projects/gwm/support/customSteps.js
@@ -8,7 +8,5 @@ Cypress.Commands.add('TESTlogin',() => {
       cy.get(GWM_ELEMENT.longinUsername).type(data.username)
       cy.get(GWM_ELEMENT.loginPassword).type(data.password)
       cy.get(GWM_ELEMENT.loginbutton).click()
-      cy.url().should('include','welcome')
-      cy.get('.header-a').should('contain', 'baigeng')
       })
   })
